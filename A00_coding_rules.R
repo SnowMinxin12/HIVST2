a80637b40c7a4ac513a08f2909bdb43d3aedec57
Minxin.Lu <minxin.lu@unc.edu>
2021-03-11 12:23:00 -0500

    first meeting

---
+++ A00_coding_rules.R
@@ -0,0 +1,18 @@
+#### basic coding rules
+
+## 1. file naming
+##    Function files start with F
+##    Code files start with C
+##    Numbers indicate the flow of codes
+##      0x = data reading
+##      1x = data screening
+##      3x = model building and analysis
+##      (to be added)
+
+## 2. working directory
+##    Working directory is the root of the R project (HIVST2)
+##    on your local directory, create 3 folders "script", "data", "output" under the project root directory
+##    store data under the file named "data", R scripts under "script", your results under "output"
+
+## 3. rule
+##    Binary variables: 0=Yes, 1=No
