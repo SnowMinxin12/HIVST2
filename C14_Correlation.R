4fa3c4cfba363fa2de12533dfbd96692ae180c9b
Minxin.Lu <minxin.lu@unc.edu>
2021-03-23 18:22:17 -0400

    Update C14_Correlation.R

--- C14_Correlation.R
+++ C14_Correlation.R
@@ -39,13 +39,13 @@ dataB.c14$U <- as.numeric(dataB.c14$U)
 
 
 # change factors to dummies
-dummyB.c14 <- fastDummies::dummy_cols(dataB.c14, remove_first_dummy = TRUE)
+dummyB.c14 <- fastDummies::dummy_cols(dataB.c14, remove_first_dummy = TRUE, ignore_na=TRUE)
 dummyB.c14 <- dummyB.c14 %>% select(-all_of(c(factor_varB,ordinal_varB)))
 
 # remove zero variance varibales
-nearZeroVarName <- nearZeroVar(numericB.c14,names = TRUE)
+nearZeroVarName <- nearZeroVar(dummyB.c14,names = TRUE)
 dummyB.c14.cleaned <- dummyB.c14 %>% select(-all_of(c(nearZeroVarName)))
-  
+
 ##### factors to numbers #####
 numericB.c14 <- dummyB.c14.cleaned
 numericB.c14 = apply(numericB.c14,2,as.numeric)
@@ -61,24 +61,40 @@ corr1 <- numericB.c14 %>%
 write.csv(corr1, file = "../output/correlation.csv")
 
 ##### check for large correlation >0.8#####
-large_corr1 <- filter(corr1, (abs(value) > .8 & value != 1))
-large_corr1$value <- round(large_corr1$value,3)
-large_corr2 <- large_corr1 %>% mutate(corrName=paste(var1,"+",var2),
+large_corr1 <- filter(corr1, (abs(value) > .6 & value != 1))
+write.csv(large_corr1, file = "../output/large_corr.csv")
+
+# remove repeated correlations
+corr1_bar <- numericB.c14 %>% 
+  as.matrix %>%
+  cor(use = "pairwise.complete.obs")%>%
+  as.data.frame
+corr1_bar[lower.tri(corr1_bar,diag = TRUE)]<-"repeated"
+corr1_bar_col <- corr1_bar %>% 
+  rownames_to_column(var = 'var1') %>%
+  gather(var2, value, -var1) %>% 
+  filter(!value =="repeated")
+corr1_bar_col$value <- as.numeric(corr1_bar_col$value)
+
+# plot large correlation
+large_corr1_bar <- filter(as.data.frame(corr1_bar_col), (abs(value) > .6))
+large_corr1_bar$value <- round(large_corr1_bar$value,3)
+large_corr1_bar <- large_corr1_bar %>% mutate(corrName=paste(var1,"+",var2),
                                       sign = ifelse(value>0,"pos","neg"),
                                       absValue = abs(value))
-# plot 
-pdf("../output/large_corrplot0.8.pdf") 
-ggplot(data = large_corr2, aes(x=reorder(corrName,absValue),y=absValue,fill=sign))+
+
+pdf("../output/large_corrplot0.6.pdf") 
+ggplot(data = large_corr1_bar, aes(x=reorder(corrName,absValue),y=absValue,fill=sign))+
   geom_bar(stat="identity")+
   geom_text(aes(label=value),vjust=0.5,hjust=1.5,color="black",size=3.5)+
-  ggtitle("Variable pairs with correlation > 0.8 or <-0.8") +
+  ggtitle("Variable pairs with correlation > 0.6 or <-0.6") +
   xlab("Variable pairs") + ylab("correlation")+
   coord_flip()
 dev.off()
-write.csv(large_corr1, file = "../output/large_corr.csv")
+
 
 ##### check for correlation with DD #####
-DD_corr1 <-corr1 %>% filter(var1=="DD", var2!="DD")
+DD_corr1 <-corr1 %>% filter(var1=="DD",var2!="DD")
 DD_corr1$value <- round(DD_corr1$value,3)
 DD_corr2 <- DD_corr1 %>% mutate(corrName=paste(var1,"+",var2),
                                       sign = ifelse(value>0,"pos","neg"),
@@ -89,18 +105,20 @@ pdf("../output/corrplot_DD.pdf")
 ggplot(data = DD_corr3, aes(x=reorder(corrName,absValue),y=absValue,fill=sign))+
   geom_bar(stat="identity")+
   geom_text(aes(label=value),vjust=0.5,hjust=1.5,color="black",size=3)+
-  ggtitle("Correlation between actual distribution and other variables") +
+  ggtitle("Correlation between actual distribution and other variables >0.2") +
   xlab("Variable pairs") + ylab("correlation")+
   coord_flip()
 dev.off()
 write.csv(DD_corr1, file = "../output/corr_DD.csv")
 
 ##### check with individual variables #####
-summary(dataB.c14$Y)
-summary(dataB.c14$X)
-table(dataB.c14$W)
-check = dummyB.c14.cleaned %>% select(Y_3,AB_3)
-table(dummyB.c14.cleaned$AB_3)
+na.omit(as.data.frame(numericB.c14) %>% select(Y_3,AB_3))# only 2 obs
+na.omit(as.data.frame(numericB.c14) %>% select(Y_2,AB_3)) # only 2 obs
+na.omit(as.data.frame(numericB.c14) %>% select(AC,Y_3)) # only 2 obs
+na.omit(as.data.frame(numericB.c14) %>% select(AC,Y_2)) # only 2 obs
+na.omit(as.data.frame(numericB.c14) %>% select(AA,Y_3)) # only 2 obs
+na.omit(as.data.frame(numericB.c14) %>% select(AA,Y_2)) # only 2 obs
+dim(na.omit(as.data.frame(numericB.c14) %>% select(Q_3,T_3))) #reasonable
 
-summary(dataB.c14$T)
-summary(dataB.c14$Q)
+check = na.omit(as.data.frame(numericB.c14) %>% select(DD,CX))
+cor(check)
