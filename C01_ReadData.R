78cde29518d3ff0f06ca873f11e906c69c51228f
Zhentao20 <80295942+Zhentao20@users.noreply.github.com>
2021-03-25 23:03:46 -0400

    Update C01_ReadData.R

--- C01_ReadData.R
+++ C01_ReadData.R
@@ -4,9 +4,12 @@
 ### date: 2021-3-22
 ### input: 309index_De-identified_ENG_wdistribution.csv
 ###        207survey_De-identified_ENG.csv
+###        269alters_De-identified.csv
 ### output: DataB = baseline index data
 ###         DataS = survey index data
+###         DataA = alter index data
 
 library(readr)
 DataB <- read_csv("../data/309index_De-identified_ENG_wdistribution.csv")
 DataS <- read_csv("../data/207survey_De-identified_ENG.csv")
+DataA <- read_csv("../data/269alters_De-identified.csv")
