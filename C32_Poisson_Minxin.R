95164dc1ad5af0e76d204d1f78499b67e30f312b
Minxin.Lu <minxin.lu@unc.edu>
2021-05-04 09:41:40 -0400

    Update C32_Poisson_Minxin.R

--- C32_Poisson_Minxin.R
+++ C32_Poisson_Minxin.R
@@ -75,46 +75,54 @@ importantVarB$Variable
 write.csv(matrix(importantVarB$Variable),file="../output/importantVarB.csv")
 
 ##### Aim 3: baseline+survey 207 ######
-dataBS.c32<-read.csv("../data/jointdataBS_c22_0422.csv")[,-1]
-dataBS.c32[dataBS.c32=="SKIP"]<-0
-dataBS.c32 = dataBS.c32[,!colnames(dataBS.c32)=="confirm_code"]
+dataBSA.c32<-read.csv("../data/jointdataBSA_c22_0422.csv")[,-1]
+dataBSA.c32[dataBSA.c32=="SKIP"]<-0
+dataBSA.c32 = dataBSA.c32[,colnames(dataBSA.c32) %in% c("A.prior_hiv_test_1","B.confirm_code")]
+
+dataB.c32<-read.csv("../data/jointdataB_c22_0422.csv")[,-1]
+dataB.c32[dataB.c32=="SKIP"]<-0
+
+joint = merge(dataB.c32,dataBSA.c32,by.x = "confirm_code",by.y="B.confirm_code",all.x=TRUE)
+joint$A.prior_hiv_test_1 = ifelse(is.na(joint$A.prior_hiv_test_1),0,joint$A.prior_hiv_test_1)
+joint = joint[,!colnames(joint)=="confirm_code"]
+
 # change province to be 0 and 1=not equal to 0
-dataBS.c32$B.province01 <- ifelse(dataBS.c32$B.province==0,0,1)
-dataBS.c32 = dataBS.c32[,!colnames(dataBS.c32)=="B.province"]
+joint$province01 <- ifelse(joint$province==0,0,1)
+joint = joint[,!colnames(joint)=="province"]
 # city has too many levels so remove city
-dataBS.c32 = dataBS.c32[,!colnames(dataBS.c32) %in% c("B.city",'S.health_center')]
+joint = joint[,!colnames(joint)=="city"]
 
 
 # factor variables
-factor_varBS <- c("B.arm","B.marital_status","B.sex_orientation","B.anal_sex_role",
-                  'S.most_important','S.result_mostrecent',
-                  'S.anal_sex_role','S.test_pref')
+factor_varB <- c("arm","marital_status","sex_orientation","anal_sex_role")
 
 #ordinal variables are treated as continous
-cont_varBS<-setdiff(colnames(dataBS.c32),factor_varBS)
-dataBS.c32[cont_varBS] <- lapply(dataBS.c32[cont_varBS],as.numeric)
-dataBS.c32[factor_varBS] <- lapply(dataBS.c32[factor_varBS],factor)
+cont_varB<-setdiff(colnames(joint),factor_varB)
+joint[cont_varB] <- lapply(joint[cont_varB],as.numeric)
+joint[factor_varB] <- lapply(joint[factor_varB],factor)
 
-dataBS.c32.narm<-na.omit(dataBS.c32)
+
+joint.narm<-na.omit(joint)
 
 set.seed(1)
-dataBS_elnet_cv2 = train(
-  B.test_kits_actual ~ .-B.test_kits_request, 
-  data = dataBS.c32.narm,
+cv_5 = trainControl(method = "cv", number = 5)
+joint.narm_elnet_cv2 = train(
+  A.prior_hiv_test_1 ~ .-test_kits_request-test_kits_actual, 
+  data = joint.narm,
   family = "poisson",
   method = "glmnet",
   trControl = cv_5
 )
 
-dataBS_elnet2 <- glmnet(x = data.matrix(subset(dataBS.c32.narm,select=-c(B.test_kits_actual,B.test_kits_request))), 
-                       y = matrix(dataBS.c32.narm$B.test_kits_actual,ncol=1),
+joint.narm_elnet2 <- glmnet(x = data.matrix(subset(joint.narm,select=-c(A.prior_hiv_test_1,test_kits_request,test_kits_actual))), 
+                       y = matrix(joint.narm$A.prior_hiv_test_1,ncol=1),
                        family = "poisson", 
-                       lambda = dataBS_elnet_cv2$bestTune$lambda, 
-                       alpha =dataBS_elnet_cv2$bestTune$alpha)
+                       lambda = joint.narm_elnet_cv2$bestTune$lambda, 
+                       alpha =joint.narm_elnet_cv2$bestTune$alpha)
 
-importantVarBS = data.frame(dataBS_elnet2$beta[,1])
-importantVarBS$Variable = rownames(importantVarBS)
-colnames(importantVarBS)=c("beta","Variable")
-importantVarBS = importantVarBS %>% filter(beta!=0)
-importantVarBS$Variable
-write.csv(matrix(importantVarB$Variable),file="../output/importantVarB.csv")
+importantVarNewAlter = data.frame(joint.narm_elnet2$beta[,1])
+importantVarNewAlter$Variable = rownames(importantVarNewAlter)
+colnames(importantVarNewAlter)=c("beta","Variable")
+importantVarNewAlter = importantVarNewAlter %>% filter(beta!=0)
+importantVarNewAlter$Variable
+write.csv(matrix(importantVarNewAlter$Variable),file="../output/importantVar_Aim3.csv")
